b138810f4c1b5fb76e16952fdd536224bed7ea39
coolkidneversleep <iiippbhy@gmail.com>
2021-08-17 19:39:29 +0700

    fix link to anoother page

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,5 @@
 import styles from "../styles/About.module.css";
+import Link from 'next/link'
 const Nav =() =>{
     return(
         <div className={styles.nav_container}>
@@ -7,9 +8,9 @@ const Nav =() =>{
                 <div className={styles.name} style={{color:"#F95511",paddingLeft:"1vw"}}>PogPog</div>
             </div>
             <div className={styles.name_div}>
-                <a className={styles.menu_txt} href="/">Home</a>
-                <a className={styles.menu_txt} href="/about">About</a>
-                <a className={styles.demo_btn} href="/"> Demo</a>
+                <Link href="/"><a className={styles.menu_txt}>Home</a></Link>
+                <Link href="/about"><a className={styles.menu_txt}>About</a></Link>
+                <Link href="/"><a className={styles.demo_btn}> Demo</a></Link>
             </div>
             
         </div>
