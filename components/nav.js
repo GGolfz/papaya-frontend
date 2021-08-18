b766727400554ee517e87ab300b2288114c3d056
GGolfz <wisarutgolf11@gmail.com>
2021-08-18 22:36:31 +0700

    fix: nav :hammer:

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,28 @@
 import styles from "../styles/About.module.css";
-import Link from 'next/link'
-const Nav =() =>{
-    return(
-        <div className={styles.nav_container}>
-            <div className={styles.name_div}>
-                <div className={styles.name}>Papaya</div>
-                <div className={styles.name} style={{color:"#F95511",paddingLeft:"1vw"}}>PogPog</div>
-            </div>
-            <div className={styles.name_div}>
-                <Link href="/"><a className={styles.menu_txt}>Home</a></Link>
-                <Link href="/about"><a className={styles.menu_txt}>About</a></Link>
-                <Link href="/"><a className={styles.demo_btn}> Demo</a></Link>
-            </div>
-            
-        </div>
-    )
-}
+import Link from "next/link";
+const Nav = () => {
+  return (
+    <div className={styles.nav_container}>
+      <div className={styles.name_div}>
+        <span>Papaya</span>
+        <span
+          className={styles.special}
+        >
+          PogPog
+        </span>
+      </div>
+      <div className={styles.action_div}>
+        <Link href="/">
+          <a className={styles.menu_txt}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={styles.menu_txt}>About</a>
+        </Link>
+        <Link href="/demo">
+          <a className={styles.demo_btn}> Demo</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default Nav;
