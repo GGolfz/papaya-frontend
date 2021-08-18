a7d2ef9ed3a0c6ce509a98b76c4b05c4752ca81c
GGolfz <wisarutgolf11@gmail.com>
2021-08-18 22:39:16 +0700

    add: nav to demo :sparkles:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Fragment } from "react";
+import Head from 'next/head'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Papaya PogPog</title>
+        <meta name="description" content="Papaya PogPog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/demo.js
+++ pages/demo.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
 import { useState, useEffect, Fragment } from "react";
 import UploadIcon from "../components/icons/upload";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import Dialog from "@material-ui/core/Dialog";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Nav from "../components/nav";
 const Demo = () => {
   const [loading, setLoading] = useState(false);
   const [predict, setPredict] = useState(null);
@@ -46,60 +46,64 @@ const Demo = () => {
     }
   };
   return (
+    <Fragment>
     <div className={styles.container}>
-      <Head>
-        <title>Papaya PogPog</title>
-        <meta name="description" content="Papaya PogPog" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Nav />
+        <main className={styles.main}>
+          <Fragment>
+            <h1 className={styles.title}>
+              Welcome to <a href="#">Papaya Pog Pog!</a>
+            </h1>
 
-      <main className={styles.main}>
-        <Fragment>
-          <h1 className={styles.title}>
-            Welcome to <a href="#">Papaya Pog Pog!</a>
-          </h1>
-
-          <p className={styles.description}>Get started by upload your image</p>
-          <input
-            type="file"
-            style={{ display: "none" }}
-            id="fileUpload"
-            onChange={handleFileUpload}
-          />
-          {!loading && img && predict ? (
-            <Dialog open={dialog} onBackdropClick={() => setDialog(false)}>
-              <img src={img} alt="Papaya Image" width="224px" height="224px" />
-              <h3>
-                I am {Math.round(predict.confident * 10000) / 100}% confident
-                that your papaya are {predict.class} papaya.
-              </h3>
-            </Dialog>
-          ) : null}
-          <div className={styles.grid} onClick={handleClickUpload}>
-            <div className={styles.card}>
-              {loading ? (
-                <CircularProgress size={60} />
-              ) : (
-                <Fragment>
-                  <UploadIcon />
-                  <h2>Upload Papaya Image </h2>
-                </Fragment>
-              )}
+            <p className={styles.description}>
+              Get started by upload your image
+            </p>
+            <input
+              type="file"
+              style={{ display: "none" }}
+              id="fileUpload"
+              onChange={handleFileUpload}
+            />
+            {!loading && img && predict ? (
+              <Dialog open={dialog} onBackdropClick={() => setDialog(false)}>
+                <img
+                  src={img}
+                  alt="Papaya Image"
+                  width="224px"
+                  height="224px"
+                />
+                <h3>
+                  I am {Math.round(predict.confident * 10000) / 100}% confident
+                  that your papaya are {predict.class} papaya.
+                </h3>
+              </Dialog>
+            ) : null}
+            <div className={styles.grid} onClick={handleClickUpload}>
+              <div className={styles.card}>
+                {loading ? (
+                  <CircularProgress size={60} />
+                ) : (
+                  <Fragment>
+                    <UploadIcon />
+                    <h2>Upload Papaya Image </h2>
+                  </Fragment>
+                )}
+              </div>
             </div>
-          </div>
-        </Fragment>
-      </main>
+          </Fragment>
+        </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://ggolfz.codes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <span className={styles.logo}>GGolfz</span>
-        </a>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <a
+            href="https://ggolfz.codes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by <span className={styles.logo}>GGolfz</span>
+          </a>
+        </footer>
+      </div>
+    </Fragment>
   );
 };
 export default Demo;
