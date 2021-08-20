2a8429c912a94c94cbc63de84feeba5421f85ae2
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-08-20 23:10:26 +0700

    fix: about page

--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,28 @@
 import Nav from "../components/nav";
 import SystemArch from "../components/system_arc";
 import DevTeam from "../components/developer_team";
-import { mockTech } from '../components/mockTech'
+import { mockTech } from "../components/mockTech";
 import { Fragment } from "react";
-import TechStack from '../components/tech_stack';
-import styles from '../styles/About.module.css';
+import TechStack from "../components/tech_stack";
+import styles from "../styles/About.module.css";
 const About = () => {
   const renderTech = () => {
-    const tech = mockTech.map((item) =>{
-      return <TechStack key={item.id} name={item.name} img={item.img}></TechStack>
-      
+    const tech = mockTech.map((item) => {
+      return (
+        <TechStack key={item.id} name={item.name} img={item.img}></TechStack>
+      );
     });
     return tech;
-  }
+  };
   return (
     <Fragment>
       <Nav />
       <SystemArch />
       <div className={styles.tech_stacks}>
-      <div className={styles.tech_name}>
-        Technology Stacks
-      </div>
-      <div className={styles.flexTech}>
-      {renderTech()}
-      </div>
+        <div className={styles.tech_name}>Technology Stacks</div>
+        <div className={styles.flexTech}>{renderTech()}</div>
       </div>
       <DevTeam />
-      
     </Fragment>
   );
 };
