b40ec1978cadee6cb0527758c3d0f85471dd53b3
KavisaraSrisuwatcharee <kavisara.noya@mail.kmutt.ac.th>
2021-08-20 22:31:07 +0700

    add: finish tech stacks

---
+++ components/mockTech.js
@@ -0,0 +1,37 @@
+exports.mockTech = [
+  {
+    id: 1,
+    name: "Python3",
+    img: "/images/technology/python.png",
+  },
+  {
+    id: 2,
+    name: "Numpy Library",
+    img: "/images/technology/numpy.png",
+  },
+  {
+    id: 3,
+    name: "Pandas Library",
+    img: "/images/technology/pandas.png",
+  },
+  {
+    id: 4,
+    name: "Tensorflow",
+    img: "/images/technology/tensorflow.png",
+  },
+  {
+    id: 5,
+    name: "Python Notebook Platform",
+    img: "/images/technology/python_note.png",
+  },
+  {
+    id: 6,
+    name: "Matplotlib Library",
+    img: "/images/technology/matplotlib.png",
+  },
+  {
+    id: 7,
+    name: "ScikitLearn Library",
+    img: "/images/technology/scikitlearn.png",
+  },
+];
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,32 @@
 import Nav from "../components/nav";
 import SystemArch from "../components/system_arc";
 import DevTeam from "../components/developer_team";
+import { mockTech } from '../components/mockTech'
 import { Fragment } from "react";
-
+import TechStack from '../components/tech_stack';
+import styles from '../styles/About.module.css';
 const About = () => {
+  const renderTech = () => {
+    const tech = mockTech.map((item) =>{
+      return <TechStack key={item.id} name={item.name} img={item.img}></TechStack>
+      
+    });
+    return tech;
+  }
   return (
     <Fragment>
       <Nav />
       <SystemArch />
+      <div className={styles.tech_stacks}>
+      <div className={styles.tech_name}>
+        Technology Stacks
+      </div>
+      <div className={styles.flexTech}>
+      {renderTech()}
+      </div>
+      </div>
       <DevTeam />
+      
     </Fragment>
   );
 };
