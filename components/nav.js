1caa4ad7f9136f9315e9b8887c759d2c4c5250fa
GGolfz <wisarutgolf11@gmail.com>
2021-08-29 00:51:51 +0700

    add: responsive navbar :sparkles:

--- components/nav.js
+++ components/nav.js
@@ -1,17 +1,23 @@
-import styles from "../styles/About.module.css";
+import styles from "../styles/Nav.module.css";
 import Link from "next/link";
+import { useState } from "react";
 const Nav = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className={styles.nav_container}>
       <div className={styles.name_div}>
         <span>Papaya</span>
-        <span
-          className={styles.special}
-        >
-          PogPog
-        </span>
+        <span className={styles.special}>PogPog</span>
       </div>
-      <div className={styles.action_div}>
+      <div
+        className={`${styles.hamburger} ${open ? styles.open : ""}`}
+        onClick={() => setOpen(!open)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`${styles.action_div} ${open ? styles.open : ""}`}>
         <Link href="/">
           <a className={styles.menu_txt}>Home</a>
         </Link>
